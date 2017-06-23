0a53cb94be26c838ee932731f7dcb2dd52f7ec92
Marni Gross <Marni@Marnis-MacBook-Pro.local>
2017-06-22 21:44:05 -0400

    cloze cards

--- ClozeCard.js
+++ ClozeCard.js
@@ -5,20 +5,22 @@ var inquirer = require("inquirer")
 function ClozeCard(text, cloze){
 	this.text = text;
 	this.cloze = cloze;
+	this.partialCard = function(){
+		 this.partialText = this.text.replace(this.cloze, "...");
+		 console.log(this.partialText)
+		 	return this.partialText
 
+
+	}
 }
 
-ClozeCard.prototype.partial = function(){
-	var partialText = this.text.replace(this.cloze, "...");
-	console.log(partialText);
+
 		
-	}
 
+// var newCard  = new ClozeCard("DNA stands for deoxyribonucleic acid ", "deoxyribonucleic acid");
+// 	var partialText = newCard.partial();
 
-var newCard  = new ClozeCard("DNA stands for deoxyribonucleic acid ", "deoxyribonucleic acid");
-	newCard.partial();
-
-// 
+// // 
 
 
 
@@ -76,66 +78,99 @@ var newCard  = new ClozeCard("DNA stands for deoxyribonucleic acid ", "deoxyribo
 
 
 
-// fs.readFile("cloze.json", "UTF8", function(err, data){
-// 	if(err) {
-// 		console.log(err);
-// 	}
-
-// 	var cardArr = JSON.parse(data);
-// 	var count = 0;
-// 	var i = 0;
-
-// // for(var k=0; k <cardArr.basic.length; k++){
+fs.readFile("cloze.json", "UTF8", function(err, data){
+	if(err) {
+		console.log(err);
+	}
 
 
-// 	function createCard(){
 
-// 		if(count<cardArr.cloze.length){
+	var clCards = []
+	var cardArr = JSON.parse(data);
 
-// 			for(var x=0; x< cardArr.cloze.length; x++){
-// 				cardArr.cloze[x].partial();
-// 			}
+	
+		// for(x = 0; x < cardArr.cloze.length; x++){
 			
+		// 	var card  = new ClozeCard(cardArr.cloze[x])
+		// 	clCards.push(card);
+			
+		// }
+	var count = 0;
+	var i = 0;
+
+
+// for(var k=0; k <cardArr.basic.length; k++){
+
 		
 	
-// 			inquirer.prompt([
 
-// 			{
-// 				name: "question",
-// 				message: partialText,
-// 				type: "input"
-// 			}	
-// 		]).then(function(answer){
+
+	function createCard(){
+
+		
+
+		if(count < cardArr.cloze.length){
+			// console.log(cardArr.cloze); 
+			// console.log('I:' + i)
+			console.log(cardArr.cloze[i])
+			
+				 var newCard = new ClozeCard(cardArr.cloze[i].text, cardArr.cloze[i].cloze);
+				 var partial = newCard.partialCard();
+				console.log(partial)
+			 
+			console.log('=====================')
 
 				
 
 
-// 				if(answer.question == cardArr.cloze[i].cloze){
-// 					console.log("You are correct");
-// 					console.log("----------------------------")
-// 				}
-// 				else{
-// 					console.log("That's incorrect");
-// 					console.log("The correct answer is " + cardArr.cloze[i].cloze)
-// 					console.log("----------------------------")
+			// console.log(typeof cardArr.cloze[i].partialCard)
+			// cardArr.cloze[i].partialCard();
+		
+		
+			
+			inquirer.prompt([
+			 	
 
-// 				}
+				{
+					name: "question",
+					message: partial,
+					type: "input"
+				}	
+			
+		]).then(function(answer){
 
-// 				count++
 
-// 				i++
+				if(answer.question == cardArr.cloze[i].cloze){
+					console.log("You are correct");
+					console.log("----------------------------")
+				}
+				else{
+					console.log("That's incorrect");
+					console.log("The correct answer is " + cardArr.cloze[i].cloze)
+					console.log("----------------------------")
 
-// 				createCard();
+				}
 
-// 		});
+				count++
+
+				i++
+
+				createCard();
+
+		});
 	
-// 	}
-// // }
 // }
 
-// createCard();
 	
-// });
+	
+}
+
+	}
+	createCard();
+
+});
+
+
 
 
 
